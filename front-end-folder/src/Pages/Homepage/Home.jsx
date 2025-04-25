49f79e8b818cef61ab1fbbc89e9fefc0db1700d9
Maya-Bishani <mayabishani6@gmail.com>
2025-04-25 15:13:25 +0300

    add files

--- front-end-folder/src/Pages/Homepage/Home.jsx
+++ front-end-folder/src/Pages/Homepage/Home.jsx
@@ -1,12 +1,13 @@
 import Section1 from "./Section1"
 import Section2 from "./Section2"
 import Section3 from "./Section3"
-
+import Footer from "../../Components/Footer"
 export default function Home(){
     return<>
    <Section1/>
    <Section2/>
    <Section3/>
+   <Footer/>
    
   
     </>
